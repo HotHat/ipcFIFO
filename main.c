662c3c85702a7ff109a9334a0256d21b1be32d14
lucasnogrib <lucasnogrib@gmail.com>
2012-12-29 23:35:33 -0300

    initial version

---
+++ main.c
@@ -0,0 +1,18 @@
+/* 
+ * File:   main.c
+ * Author: lucas
+ *
+ * Created on 29 décembre 2012, 23:34
+ */
+
+#include <stdio.h>
+#include <stdlib.h>
+
+/*
+ * 
+ */
+int main(int argc, char** argv) {
+
+    return (EXIT_SUCCESS);
+}
+
